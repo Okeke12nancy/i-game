45ae753cf9e7bf7bc32cd5f25bbaa225cd50104f
Nancy Okeke <ogadimmanancy12@gmail.com>
2025-08-05 19:10:01 +0100

    fix: fixed bugs with supabase

--- server-side/src/config/supabaseConfig.ts
+++ server-side/src/config/supabaseConfig.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl: string = 'https://sgycieotwvcrdswrsuck.supabase.co';
-const supabaseKey: string = 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNneWNpZW90d3ZjcmRzd3JzdWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MDUyMzgsImV4cCI6MjA2OTk4MTIzOH0.ipulWLzbOIG8gqeTgDWraMbn1XWUgONlYHm9RSPwcz0';
+const supabaseKey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNneWNpZW90d3ZjcmRzd3JzdWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MDUyMzgsImV4cCI6MjA2OTk4MTIzOH0.ipulWLzbOIG8gqeTgDWraMbn1XWUgONlYHm9RSPwcz0';
 
 let supabase: any = null;
 
